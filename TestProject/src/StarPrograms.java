1ac86e06d446a815b82b4ea3ba3565bd4b8c6b83
kiran orsu <kiran orsu@192.168.0.6>
2020-07-23 22:19:34 +0530

    after  extracting code into 3 methods.

--- TestProject/src/StarPrograms.java
+++ TestProject/src/StarPrograms.java
@@ -7,46 +7,57 @@ public class StarPrograms {
 				righttraingle();
 				
 				//star pattern 5
-			       for (int row=1; row<=4; row++)
-			       {
-			    	   for(int column=2; column<=row; column++)
-			    	   {
-			    		   System.out.print(" ");
-			    	   }
-			    	  for ( int secondRow=4; secondRow>=row; secondRow--)
-			    	  {
-			    		System.out.print("*");  
-			    	  }
-			    	  System.out.println();
-			       }
-			       System.out.println();
+			    reversetraingle();
 			       
-			       //star pattern 6
-			       for (int row=1; row<=4; row++)
-			       {
-			    	   for(int column=3; column>=row; column--)
-			    	   {
-			    		   System.out.print(" ");
-			    	   }
-			    	  for (int secondRow=1; secondRow<=row; secondRow++)
-			    	  {
-			    		System.out.print("*");  
-			    	  }
-
-			            for (int secondColumn=2; secondColumn<=row; secondColumn++)    //here 'secondColumn' is for another iteration of new column
-			            { 
-					    		 System.out.print("*");   
-			            }
-			               System.out.println();
-			         }
+			    //star pattern 6
+			    pyramid();
 
 	}
 
-	private static void righttraingle() {
+	private static void pyramid() 
+	{
+		for (int row=1; row<=4; row++)
+		   {
+			   for(int column=3; column>=row; column--)
+			   {
+				   System.out.print(" ");
+			   }
+			  for (int secondRow=1; secondRow<=row; secondRow++)
+			  {
+				System.out.print("*");  
+			  }
+
+		        for (int secondColumn=2; secondColumn<=row; secondColumn++)    //here 'secondColumn' is for another iteration of new column
+		        { 
+			    		 System.out.print("*");   
+		        }
+		           System.out.println();
+		     }
+	}
+
+	private static void reversetraingle() 
+	{
+		for (int row=1; row<=4; row++)
+		   {
+			   for(int column=2; column<=row; column++)
+			   {
+				   System.out.print(" ");
+			   }
+			  for ( int secondRow=4; secondRow>=row; secondRow--)
+			  {
+				System.out.print("*");  
+			  }
+			  System.out.println();
+		   }
+		   System.out.println();
+	}
+
+	private static void righttraingle() 
+	{
 		for(int row=1; row<=4; row++)
 		{
  
-		   for(int column=3; column>=row; column--)
+		    for(int column=3; column>=row; column--)
 		    {   
 		        System.out.print(" ");
 		    }
