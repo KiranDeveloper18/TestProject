40520199f1449c1ef2ceac9d212dd5d797cfcd78
kiran orsu <kiran orsu@192.168.0.5>
2020-08-27 22:07:14 +0530

    about fibonacci by using tail recursion and loop.

---
+++ TestProject/src/FibonacciSeries.java
@@ -0,0 +1,61 @@
+
+
+public class FibonacciSeries 
+{
+	//using tailrecursive
+	
+	public static int fibonacci(int number)
+	{
+		if (number == 1 || number == 2)
+		{
+			return 1;
+		}
+		
+		return fibonacci(number - 1) + fibonacci(number - 2);
+	}
+	
+	
+	// using loop or iteration
+	
+	public static int fibonacci2(int number)
+	{ 
+		if(number == 1 || number == 2)
+		{ 
+			return 1;
+		} 
+		
+		int fibo1 = 1, fibo2 = 1, fibonacci = 1; 
+		
+		for(int i= 3; i<= number; i++)
+		{ 
+			
+			//Fibonacci number is sum of previous two Fibonacci number 
+			
+			fibonacci = fibo1 + fibo2; 
+			
+			fibo1 = fibo2; 
+			
+			fibo2 = fibonacci; 
+			
+		} 
+		return fibonacci; 
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+		int number = 12;
+		
+		System.out.println("Enter the number upto which Fibonacci series to print : " + number );
+		
+		System.out.println();
+		
+		System.out.println("Fibonacci series upto " + number + " numbers: ");
+		
+		for ( int i = 1; i<=number; i++)
+		{
+			System.out.print(fibonacci2(i) + "   ");
+		}
+	}
+
+}
